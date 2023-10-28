cbb9997b84a822a968dd1b7d06dd66d8ced45e05
Niklasss98 <nlucca98@gmail.com>
2023-10-28 19:05:03 -0300

    Imagenes

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -3,11 +3,12 @@ import Carousel from 'react-bootstrap/Carousel';
 const CarouselHome = () => {
   return (
     <Carousel>
+      {/* No pude linkear las imágenes que se ecuentran en la carpeta 'src/images', después probar y arreglar en lo posible */}
     <Carousel.Item>
         < img
         className='d-block w-100'
         style={{maxHeight:"500px", objectFit:'cover'}}
-            src="/src/assets/images/slide1.jpg" alt="slide1" />
+            src="https://images.pexels.com/photos/220067/pexels-photo-220067.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="slide1" />
       <Carousel.Caption>
         <h3>Primer Slide</h3>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -19,7 +20,7 @@ const CarouselHome = () => {
     < img
         className='d-block w-100'
         style={{maxHeight:"500px", objectFit:'cover'}}
-            src="/src/assets/images/slide2.jpg" alt="slide2" />
+            src="https://images.pexels.com/photos/952670/pexels-photo-952670.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="slide2" />
       <Carousel.Caption>
         <h3>Segundo Slide</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -31,7 +32,7 @@ const CarouselHome = () => {
     < img
         className='d-block w-100'
         style={{maxHeight:"500px", objectFit:'cover'}}
-            src="/src/assets/images/slide3.jpg" alt="slide3" />
+            src="https://images.pexels.com/photos/164005/pexels-photo-164005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="slide3" />
       <Carousel.Caption>
         <h3>Tercer Slide</h3>
         <p>
